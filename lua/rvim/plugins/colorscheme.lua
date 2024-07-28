ec2dc56d9e6893e28b666e2bd2d88585dd19746e
Rosmer López <rosmerlopez99@gmail.com>
2024-07-28 03:28:28 -0400

    refactor(colorscheme): updated nord.nvim configuration

--- lua/rvim/plugins/colorscheme.lua
+++ lua/rvim/plugins/colorscheme.lua
@@ -37,13 +37,7 @@ return {
     priority = 1000,
     lazy = RVimOptions.colorscheme ~= "nord",
     config = function()
-      vim.g.nord_contrast = false
-      vim.g.nord_borders = true
-      vim.g.nord_disable_background = true
-      vim.g.nord_cursorline_transparent = false
-      vim.g.nord_enable_sidebar_background = false
       vim.g.nord_italic = false
-      vim.g.nord_uniform_diff_background = false
     end,
   },
 
