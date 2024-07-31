d69607e6bd587106883bc3d4fa7b08199d3fd3a0
Rosmer López <rosmerlopez99@gmail.com>
2024-07-31 02:14:58 -0400

    refactor(options): minor changes and improvements to settings

--- lua/rvim/core/options.lua
+++ lua/rvim/core/options.lua
@@ -1,26 +1,34 @@
 -- This file is automatically loaded
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+local g = vim.g
+
+g.mapleader = " "
+g.maplocalleader = "\\"
+
+-- Fix markdown indentation settings
+g.markdown_recommended_style = 0
 
 -- Disable perl provider
-vim.g.loaded_perl_provider = 0
+g.loaded_perl_provider = 0
 -- Disable python 3 provider
-vim.g.loaded_python3_provider = 0
+g.loaded_python3_provider = 0
 -- Disable ruby provider
-vim.g.loaded_ruby_provider = 0
+g.loaded_ruby_provider = 0
 -- Disable node provider
-vim.g.loaded_node_provider = 0
+g.loaded_node_provider = 0
 
-vim.opt.autowrite = true -- Enable auto write
+-- Setup editor options
+local opt = vim.opt
+
+opt.autowrite = true -- Enable auto write
 -- only set clipboard if not in ssh, to make sure the OSC 52
 -- integration works automatically. Requires Neovim >= 0.10.0
-vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.cursorline = true -- Enable highlighting of the current line
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.fillchars = {
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+opt.fillchars = {
   foldopen = "",
   foldclose = "",
   fold = " ",
@@ -28,23 +36,23 @@ vim.opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-vim.opt.foldlevel = 99
-vim.opt.formatoptions = "jcroqlnt" -- tcqj
-vim.opt.grepformat = "%f:%l:%c:%m"
-vim.opt.grepprg = "rg --vimgrep"
-vim.opt.ignorecase = true -- Ignore case
-vim.opt.inccommand = "nosplit" -- preview incremental substitute
-vim.opt.jumpoptions = "view"
-vim.opt.laststatus = 3 -- global statusline
-vim.opt.linebreak = true -- Wrap lines at convenient points
-vim.opt.list = true -- Show some invisible characters (tabs...
-vim.opt.mouse = "a" -- Enable mouse mode
-vim.opt.number = true -- Print line number
-vim.opt.pumblend = 10 -- Popup blend
-vim.opt.pumheight = 10 -- Maximum number of entries in a popup
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.scrolloff = 4 -- Lines of context
-vim.opt.sessionoptions = {
+opt.foldlevel = 99
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true -- Ignore case
+opt.inccommand = "nosplit" -- preview incremental substitute
+opt.jumpoptions = "view"
+opt.laststatus = 3 -- global statusline
+opt.linebreak = true -- Wrap lines at convenient points
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.relativenumber = true -- Relative line numbers
+opt.scrolloff = 4 -- Lines of context
+opt.sessionoptions = {
   "buffers",
   "curdir",
   "tabpages",
@@ -54,29 +62,28 @@ vim.opt.sessionoptions = {
   "skiprtp",
   "folds",
 }
-vim.opt.shiftround = true -- Round indent
-vim.opt.shiftwidth = 2 -- Size of an indent
-vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
-vim.opt.showmode = false -- Dont show mode since we have a statusline
-vim.opt.sidescrolloff = 8 -- Columns of context
-vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.smartindent = true -- Insert indents automatically
-vim.opt.spelllang = { "en" }
-vim.opt.spelloptions:append("noplainbuffer")
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitkeep = "screen"
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.tabstop = 2 -- Number of spaces tabs count for
-vim.opt.termguicolors = true -- True color support
-vim.opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
-vim.opt.undofile = true
-vim.opt.undolevels = 10000
-vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
-vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
-vim.opt.winminwidth = 5 -- Minimum window width
-vim.opt.wrap = false -- Disable line wrap
-
--- Fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.spelloptions:append("noplainbuffer")
+opt.splitbelow = true -- Put new windows below current
+opt.splitkeep = "screen"
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
+opt.timeoutlen = g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
+opt.undofile = true
+opt.undolevels = 10000
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.winminwidth = 5 -- Minimum window width
+opt.wrap = false -- Disable line wrap
+opt.shadafile = "" -- Disable shadafile
+opt.shell = "/bin/zsh" -- Set shell
